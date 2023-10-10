d9aeaf45ab9cf0628b1ef68f48164da2e1959ee8
vmb02 <morenobenitezvictor@gmail.com>
2023-10-10 23:21:11 +0200

    Subo formularios acabado

--- Formularios/muchos_formularios.php
+++ Formularios/muchos_formularios.php
@@ -60,9 +60,73 @@
         <label for="n3">Número 3</label>
         <br>
         <input type="number" id="n3" name="n3">
+        <br>
+        <input type="hidden" name="action" value="maximo3">
+        <input type="submit" name="Calcular">
+        <br>
+        <?php
 
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            if($_POST["action"] == "maximo3") {
+                $n1 = (int) $_POST["n1"];
+                $n2 = (int) $_POST["n2"];
+                $n3 = (int) $_POST["n3"];
+                echo maxi($n1, $n2, $n3);
+            }
+        }
+
+        ?>
     </fieldset>
 </form>
+
+<h1>FORMULARIO POTENCIA</h1>
+
+<form action="" method="post">
+    <fieldset>
+        <label for="base">Base: </label>
+        <br>
+        <input type="number" id="base" name="base">
+        <br><br>
+        <label for="exponente">Exponente: </label>
+        <br>
+        <input type="number" id="exponente" name="exponente">
+        <br>
+        <input type="hidden" name="action" value="potencia">
+        <input type="submit" name="Calcular">
+        <br>
+        <?php
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            if($_POST["action"] == "potencia") {
+                $base = (int) $_POST["base"];
+                $exponente = (int) $_POST["exponente"];
+                echo $base . " elevado a " . $exponente . " es: " . potencia($base, $exponente);
+            }
+        }
+    ?>
+    </fieldset>
+
+<h1>FORMULARIO PRIMOS</h1>
+<form action="" method="post">
+    <fieldset>
+        <label for="nPrimos">Número de primos: </label>
+        <br>
+        <input type="number" id="nPrimos" name="nPrimos">
+        <br>
+        <input type="hidden" name="action" value="primos">
+        <input type="submit" name="Calcular">
+        <br>
+        <?php
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            if($_POST["action"] == "primos") {
+                $numeroPrimos = (int) $_POST["nPrimos"];
+                echo "Los " . $numeroPrimos . " primeros números primos son: ";
+                print_r(primos($numeroPrimos));
+            }
+        }
+        ?>
+    </fieldset>
 
 </body>
 </html>
