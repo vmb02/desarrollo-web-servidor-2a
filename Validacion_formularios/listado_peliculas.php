fd52aac34acc8c6a9e2051bc258b4937e4f736a1
vmb02 <morenobenitezvictor@gmail.com>
2023-10-25 11:28:44 +0200

    Termino peliculas y añado listado_peliculas

---
+++ Validacion_formularios/listado_peliculas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de películas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require '../Funciones/conexion_peliculas.php' ?>
+</head>
+<body>
+    <div class="container">
+        <h1>Listado de películas</h1>
+    </div>
+    <div>
+        <table class="table table-striped">
+            <thead class="table table-dark">
+                <tr>
+                    <th>ID Película</th>
+                    <th>Titulo</th>
+                    <th>Fecha Estreno</th>
+                    <th>Edad recomendada</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            $sql = "SELECT * FROM peliculas";
+            $resultado = $conexion -> query($sql);
+            while($fila = $resultado -> fetch_assoc()) {
+                echo "<tr><td>" . $fila["id_pelicula"] . "</td>
+                        <td>" . $fila["titulo"] . "</td>
+                        <td>" . $fila["fecha_estreno"] . "</td>
+                        <td>" . $fila["edad_recomendada"] . "</td></tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
--- Validacion_formularios/peliculas.php
+++ Validacion_formularios/peliculas.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Peliculas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require '../Funciones/conexion_peliculas.php' ?>
 </head>
 <body>
     <?php
@@ -12,15 +14,19 @@
         $salida = trim($salida);
         return $salida;
     }
-    
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $temp_id = depurar($_POST["id"]);
         $temp_titulo = depurar($_POST["titulo"]);
         $temp_fecha_estreno = depurar($_POST["fecha_estreno"]);
-        $temp_edad_recomendada = depurar($_POST["edad_recomendada"]);
+        if(isset($_POST["edad_recomendada"])) {
+            $temp_edad_recomendada = depurar($_POST["edad_recomendada"]);
+            echo "<p>edad $temp_edad_recomendada</p>";
+        } else {
+            $temp_edad_recomendada = "";
+        }
 
         //Validación del ID
-        if(!strlen($temp_id) > 0) {
+        if(strlen($temp_id) == 0) {
             $err_id = "El ID de la película es obligatorio";
         } else {
             if(filter_var($temp_id, FILTER_VALIDATE_INT) === FALSE) {
@@ -40,7 +46,7 @@
         }
 
         //Validación del título
-        if(!strlen($temp_titulo) > 0) {
+        if(strlen($temp_titulo) == 0) {
             $err_titulo = "El título de la película es obligatorio";
         } else {
             if(strlen($temp_titulo) > 80) {
@@ -63,33 +69,75 @@
         }
 
         //Validación de la edad
-        
-
-
+        if(strlen($temp_edad_recomendada) == 0) {
+            $err_edad = "La edad recomendada es obligatoria";
+        } else {
+            $edades_recomendadas = ["0", "3", "7", "12", "16", "18"];
+            if(!in_array($temp_edad_recomendada, $edades_recomendadas)) {
+                $err_edad = "La edad recomendada no es válida";
+            } else {
+                $edad_recomendada = $temp_edad_recomendada;
+            }
+        }
+/*
+        echo "<p>$id</p>";
+        echo "<p>$titulo</p>";
+        echo "<p>$fecha_estreno</p>";
+        echo "<p>$edad_recomendada</p>";
+*/
+        if(isset($id) && isset($titulo) && isset($fecha_estreno) && isset($edad_recomendada)) {
+            echo "<h1>Éxito</h1>";
+            echo "<p>$edad_recomendada</p>";
+            $sql = "INSERT INTO peliculas
+                    VALUES ($id,
+                            '$titulo',
+                            '$fecha_estreno',
+                            '$edad_recomendada')";
+        $conexion -> query($sql);
+        }
     }
     
     ?>
-    
-    <form action="" method="post">
-        <fieldset>
-            <label>ID Película: </label>
-            <input type="number" name="id">
-            <?php if(isset($err_id)) echo $err_id ?>
-            <br><br>
-            <label>Título: </label>
-            <input type="text" name="nombre">
-            <?php if(isset($err_titulo)) echo $err_titulo ?>
-            <br><br>
-            <label>Fecha de estreno: </label>
-            <input type="date" name="fecha_estreno">
-            <?php if(isset($err_fecha_estreno)) echo $err_fecha ?>
-            <br><br>
-            <label>Edad recomendada: </label>
-            <input type="number" name="edad_recomendada">
-            <?php if(isset($err_edad)) echo $err_edad ?>
-            <br><br>
-            <input type="submit" value="Registrar">
-        </fieldset>
-    </form>
+
+<div class="container">
+    <h1>Insertar película</h1>
+    <div class="col-9">
+        <form action="" method="post">
+            <fieldset>
+                <div class="mb-3">
+                <label class="form-label">ID Película: </label>
+                <input class="form-control" type="number" name="id">
+                <?php if(isset($err_id)) echo $err_id ?>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Título: </label>
+                    <input class="form-control" type="text" name="titulo">
+                    <?php if(isset($err_titulo)) echo $err_titulo ?>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Fecha de estreno: </label>
+                    <input class="form-control" type="date" name="fecha_estreno">
+                    <?php if(isset($err_fecha_estreno)) echo $err_fecha ?>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Edad recomendada: </label>
+                    <select class="form-select" name="edad_recomendada">
+                        <option disabled selected hidden>-- Elige una edad --</option>
+                        <option value="0">Todos los públicos</option>
+                        <option value="3">Mayores de 3 años</option>
+                        <option value="7">Mayores de 7 años</option>
+                        <option value="12">Mayores de 12 años</option>
+                        <option value="16">Mayores de 16 años</option>
+                        <option value="18">Mayores de 18 años</option>
+                    </select>
+                    <?php if(isset($err_edad)) echo $err_edad ?>
+                </div>
+                <button class="btn btn-primary" type="submit">Enviar</button>
+            </fieldset>
+        </form>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
